ef5b9574911f9b70f35de5ad8a5a911afc59ad37
Jay Dinesh Rane <jayu.rane32003@gmail.com>
2024-10-15 18:03:32 +0530

    Start

---
+++ air-pollution-maps/Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MapComponent from './components/mapComponent'
+const App = () => {
+  return (
+    <div>
+
+
+<MapComponent></MapComponent>
+    </div>
+  )
+}
+
+export default App
